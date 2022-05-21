d066845330840ebef3c0022902b14611e8f3a82b
BrianWong05 <wongtiniaomacau@gmail.com>
2022-05-22 00:02:22 +0800

    updated

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ SCOPES = ["https://www.googleapis.com/auth/forms.body ", "https://www.googleapis
 DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
 
 store = file.Storage('token.json')
-creds = None
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
-    creds = tools.run_flow(flow, store)
+creds = store.get()
+# creds = None
+# if not creds or creds.invalid:
+#     flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
+#     creds = tools.run_flow(flow, store)
 
 form_service = discovery.build('forms', 'v1', http=creds.authorize(Http()), discoveryServiceUrl=DISCOVERY_DOC, static_discovery=False)
 
@@ -40,15 +41,16 @@ def create_form(docTitle="",title="",descr=""):
         ]
     }
     result = form_service.forms().create(body=FORM).execute()
+    id = result['formId']
     form_service.forms().batchUpdate(formId=id, body=setting).execute()
     return result
 
-def create_choiceQuestion(id,title="",descr="",required=True,point=0,ans=[{"value": ""}],Type="RADIO",options=[{"value": ""}],shuffle=True,idx=0):
+def create_choiceQuestion(id,question="",descr="",required=True,point=0,ans=[{"value": ""}],Type="RADIO",options=[{"value": ""}],shuffle=True,idx=0):
     QUESTION = {
         "requests": [{
             "createItem": {
                 "item": {
-                    "title": title,
+                    "title": question,
                     "description": descr,
                     "questionItem": {
                         "question": {
@@ -77,12 +79,12 @@ def create_choiceQuestion(id,title="",descr="",required=True,point=0,ans=[{"valu
     question_setting = form_service.forms().batchUpdate(formId=id, body=QUESTION).execute()
     return question_setting
 
-def create_textQuestion(id,title="",descr="",required=True,point=0,ans=[{"value": ""}],para=True,idx=0):
+def create_textQuestion(id,question="",descr="",required=True,point=0,ans=[{"value": ""}],para=True,idx=0):
     QUESTION = {
         "requests": [{
             "createItem": {
                 "item": {
-                    "title": title,
+                    "title": question,
                     "description": descr,
                     "questionItem": {
                         "question": {
@@ -114,7 +116,15 @@ def get_form(id):
     return get_result
     
 def get_responses(id):
+    form = get_form(id)
+    quest_id = {}
+    items = form['items']
+    for item in items:
+        qid = item['questionItem']['question']['questionId']
+        question = item['title']
+        quest_id[qid] = question
     res = form_service.forms().responses().list(formId=id).execute()
+
     return res
 
 def get_link(id):
